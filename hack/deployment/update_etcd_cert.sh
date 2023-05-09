3f65732cc4bf666b61df75349ff86fabd9b89176
Airren <qiang.ren@intel.com>
2023-05-09 16:55:16 +0800

    fix

--- hack/deployment/update_etcd_cert.sh
+++ hack/deployment/update_etcd_cert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export key=$(cat /etc/kubernetes/pki/etcd/server.ctr)
+export key=$(sudo cat /etc/kubernetes/pki/etcd/server.crt)
 export cert=$(cat /etc/kubernetes/pki/etcd/server.key)
 export ca=$(cat /etc/kubernetes/pki/etcd/ca.crt)
 
-yq -i '.data.server.key =  strenv(key)' ./flannel-etcd-cert.yaml
-yq -i '.data.server.crt =  strenv(cert)' ./flannel-etcd-cert.yaml
-yq -i '.data.ca.crt =  strenv(ca)' ./flannel-etcd-cert.yaml
+yq -i '.data."server.key" =  strenv(key)' /home/ubuntu/hack/deployment/flannel-etcd-cert.yaml
+yq -i '.data."server.crt"=  strenv(cert)'  /home/ubuntu/hack/deployment/flannel-etcd-cert.yaml
+yq -i '.data."ca.crt" =  strenv(ca)' /home/ubuntu/hack/deployment/flannel-etcd-cert.yaml
